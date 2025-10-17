09b84712f63da7ee4fbd75166df438e22d839487
Flavio <FlavioACampos@outlook.com>
2025-10-16 22:47:03 -0500

    chore: initialize Python package structure

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Galois Fields and Shannon-Fano Coding
+====================================
+
+This package implements Shannon-Fano coding algorithm and Galois Fields for error detection/correction.
+"""
+
+__version__ = "1.0.0"
